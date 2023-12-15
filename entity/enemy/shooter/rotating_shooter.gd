04da62c50e78f216210ccfc6f6f0da89d07c5e10
SnappyDragon64 <pg61151@gmail.com>
2023-12-16 01:10:20 +0530

    Disabled out-of-view turrets

--- entity/enemy/shooter/rotating_shooter.gd
+++ entity/enemy/shooter/rotating_shooter.gd
@@ -4,10 +4,15 @@ extends 'res://entity/enemy/brick.gd'
 @export var shoot_speed : float = 1
 @export var speed : int = 200
 
+@onready var visibility_notifier : VisibleOnScreenNotifier2D
+
 var counter : int = 0
 var direction : float = 0.0
 
 func _ready():
+	visibility_notifier = VisibleOnScreenNotifier2D.new()
+	visibility_notifier.set_rect(Rect2(-1, -1, 2, 2))
+	add_child(visibility_notifier)
 	$shoot_timer.set_wait_time(shoot_speed)
 	Signals.position_updated.connect(_set_target)
 
@@ -15,7 +20,7 @@ func _process(_delta):
 	set_rotation(direction)
 
 func _on_shoot_timer_timeout():
-	if not pattern.is_empty():
+	if visibility_notifier.is_on_screen() and not pattern.is_empty():
 		var bullet_id = pattern[counter]
 		var bullet_preload = BulletRegistry.REGISTRY[bullet_id]
 		counter = (counter + 1) % pattern.size()
--- entity/enemy/shooter/shooter.gd
+++ entity/enemy/shooter/shooter.gd
@@ -3,13 +3,18 @@ extends 'res://entity/enemy/brick.gd'
 @export var pattern : Array[BulletRegistry.BULLET_TYPE] = []
 @export var shoot_speed : float = 1
 
+@onready var visibility_notifier : VisibleOnScreenNotifier2D
+
 var counter : int = 0
 
 func _ready():
+	visibility_notifier = VisibleOnScreenNotifier2D.new()
+	visibility_notifier.set_rect(Rect2(-1, -1, 2, 2))
+	add_child(visibility_notifier)
 	$shoot_timer.set_wait_time(shoot_speed)
 
 func _on_shoot_timer_timeout():
-	if not pattern.is_empty():
+	if visibility_notifier.is_on_screen() and not pattern.is_empty():
 		var bullet_id = pattern[counter]
 		var bullet_preload = BulletRegistry.REGISTRY[bullet_id]
 		counter = (counter + 1) % pattern.size()
